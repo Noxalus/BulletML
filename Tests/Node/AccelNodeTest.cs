a09b1b0c71c696c44dafd6a5ed55f107cebdb24f
Noxalus <noxalus@gmail.com>
2016-10-03 00:11:52 +0200

    Add valid test for accel node.

--- Tests/Node/AccelNodeTest.cs
+++ Tests/Node/AccelNodeTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Xml.Schema;
+﻿using System.IO;
 using BulletML;
 using BulletML.Enums;
 using BulletML.Nodes;
@@ -13,6 +11,8 @@ namespace Tests.Node
     [Category("NodeTest")]
     public class AccelNodeTest
     {
+        #region Invalid
+
         [Test]
         public void AccelNodeEmpty()
         {
@@ -66,5 +66,111 @@ namespace Tests.Node
 
             Assert.Throws<InvalidDataException>(() => pattern.Parse(filename));
         }
+
+        #endregion
+
+        #region Valid
+
+        [Test]
+        public void AccelNodeHorizontal()
+        {
+            var filename = TestUtils.GetFilePath(@"Content\Test\Accel\Valid\AccelNodeHorizontal.xml");
+            var pattern = new BulletPattern();
+            pattern.Parse(filename);
+
+            var topNode = pattern.RootNode.FindLabelNode("top", NodeName.action) as ActionNode;
+
+            Assert.IsNotNull(topNode);
+            Assert.AreEqual(1, topNode.ChildNodes.Count);
+
+            var accelNode = topNode.ChildNodes[0] as AccelNode;
+
+            Assert.IsNotNull(accelNode);
+            Assert.AreEqual(2, accelNode.ChildNodes.Count);
+
+            var horizontalNode = accelNode.ChildNodes[0] as HorizontalNode;
+            var termNode = accelNode.ChildNodes[1] as TermNode;
+
+            Assert.IsNotNull(horizontalNode);
+            Assert.IsNotNull(termNode);
+        }
+
+        [Test]
+        public void AccelNodeVertical()
+        {
+            var filename = TestUtils.GetFilePath(@"Content\Test\Accel\Valid\AccelNodeVertical.xml");
+            var pattern = new BulletPattern();
+            pattern.Parse(filename);
+
+            var topNode = pattern.RootNode.FindLabelNode("top", NodeName.action) as ActionNode;
+
+            Assert.IsNotNull(topNode);
+            Assert.AreEqual(1, topNode.ChildNodes.Count);
+
+            var accelNode = topNode.ChildNodes[0] as AccelNode;
+
+            Assert.IsNotNull(accelNode);
+            Assert.AreEqual(2, accelNode.ChildNodes.Count);
+
+            var verticalNode = accelNode.ChildNodes[0] as VerticalNode;
+            var termNode = accelNode.ChildNodes[1] as TermNode;
+
+            Assert.IsNotNull(verticalNode);
+            Assert.IsNotNull(termNode);
+        }
+
+        [Test]
+        public void AccelNodeHorizontalVertical()
+        {
+            var filename = TestUtils.GetFilePath(@"Content\Test\Accel\Valid\AccelNodeHorizontalVertical.xml");
+            var pattern = new BulletPattern();
+            pattern.Parse(filename);
+
+            var topNode = pattern.RootNode.FindLabelNode("top", NodeName.action) as ActionNode;
+
+            Assert.IsNotNull(topNode);
+            Assert.AreEqual(1, topNode.ChildNodes.Count);
+
+            var accelNode = topNode.ChildNodes[0] as AccelNode;
+
+            Assert.IsNotNull(accelNode);
+            Assert.AreEqual(3, accelNode.ChildNodes.Count);
+
+            var horizontalNode = accelNode.ChildNodes[0] as HorizontalNode;
+            var verticalNode = accelNode.ChildNodes[1] as VerticalNode;
+            var termNode = accelNode.ChildNodes[2] as TermNode;
+
+            Assert.IsNotNull(horizontalNode);
+            Assert.IsNotNull(verticalNode);
+            Assert.IsNotNull(termNode);
+        }
+
+        [Test]
+        public void AccelNodeVerticalHorizontal()
+        {
+            var filename = TestUtils.GetFilePath(@"Content\Test\Accel\Valid\AccelNodeVerticalHorizontal.xml");
+            var pattern = new BulletPattern();
+            pattern.Parse(filename);
+
+            var topNode = pattern.RootNode.FindLabelNode("top", NodeName.action) as ActionNode;
+
+            Assert.IsNotNull(topNode);
+            Assert.AreEqual(1, topNode.ChildNodes.Count);
+
+            var accelNode = topNode.ChildNodes[0] as AccelNode;
+
+            Assert.IsNotNull(accelNode);
+            Assert.AreEqual(3, accelNode.ChildNodes.Count);
+
+            var verticalNode = accelNode.ChildNodes[0] as VerticalNode;
+            var horizontalNode = accelNode.ChildNodes[1] as HorizontalNode;
+            var termNode = accelNode.ChildNodes[2] as TermNode;
+
+            Assert.IsNotNull(verticalNode);
+            Assert.IsNotNull(horizontalNode);
+            Assert.IsNotNull(termNode);
+        }
+
+        #endregion
     }
 }
