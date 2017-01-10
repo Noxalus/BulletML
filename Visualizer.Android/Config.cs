802a4469ad3f131aa89e87af68de33c5d46ffb93
Noxalus <noxalus@gmail.com>
2017-01-10 23:51:42 +0100

    Add a visualizer project for Android that use BulletML library.

---
+++ Visualizer.Android/Config.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Visualizer.Android
+{
+    public static class Config
+    {
+        public static Vector2 GameAeraSize = new Vector2(1000f, 1000f);
+    }
+}
